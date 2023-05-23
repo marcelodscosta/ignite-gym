370858d64eb9a56279d29cc710e76e3f2f53ac55
Marcelo Costa <marcelodscosta@yahoo.com.br>
2023-05-23 14:43:37 -0300

    configuring application statusbar and Assets

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, StatusBar } from "react-native";
 import {
   Roboto_400Regular,
   Roboto_700Bold,
@@ -8,7 +8,19 @@ import {
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#202024",
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Text>Hello World!</Text> : <View />}
     </View>
   );
