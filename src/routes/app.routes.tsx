bcda4e3985f60015a42f9f3b9a85f03de508ea7c
Marcelo Costa <marcelodscosta@yahoo.com.br>
2023-05-25 20:38:14 -0300

    Private Routes - Styling the Bottom Navigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,3 +1,5 @@
+import { Platform } from "react-native";
+
 import {
   BottomTabNavigationProp,
   createBottomTabNavigator,
@@ -36,6 +38,13 @@ export const AppRoutes = () => {
         tabBarShowLabel: false,
         tabBarActiveTintColor: colors.green[500],
         tabBarInactiveTintColor: colors.gray[200],
+        tabBarStyle: {
+          backgroundColor: colors.gray[600],
+          borderTopWidth: 0,
+          height: Platform.OS === "android" ? "auto" : 96,
+          paddingBottom: sizes[10],
+          paddingTop: sizes[6],
+        },
       }}
     >
       <Screen
@@ -65,7 +74,11 @@ export const AppRoutes = () => {
           ),
         }}
       />
-      <Screen name="exercise" component={Exercise} />
+      <Screen
+        name="exercise"
+        component={Exercise}
+        options={{ tabBarButton: () => null }}
+      />
     </Navigator>
   );
 };
