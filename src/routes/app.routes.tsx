6a7df836f51fb3dbe1c3974c857a4ca9c64c5801
Marcelo Costa <marcelodscosta@yahoo.com.br>
2023-05-25 19:55:58 -0300

    Private Routes - Creating App Routes

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,19 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { Home } from "@screens/Home";
+import { Exercise } from "@screens/Exercise";
+import { History } from "@screens/History";
+import { Profile } from "@screens/Profile";
+
+const { Screen, Navigator } = createBottomTabNavigator();
+
+export const AppRoutes = () => {
+  return (
+    <Navigator>
+      <Screen name="home" component={Home} />
+      <Screen name="history" component={History} />
+      <Screen name="profile" component={Profile} />
+      <Screen name="exercise" component={Exercise} />
+    </Navigator>
+  );
+};
