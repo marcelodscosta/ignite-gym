e64064ee74653c63700fc148bbc9916fc795665f
Marcelo Costa <marcelodscosta@yahoo.com.br>
2023-05-25 17:26:19 -0300

    Public Routes - Typing the Routes

--- App.tsx
+++ App.tsx
@@ -10,6 +10,8 @@ import { THEME } from "./src/theme";
 import { Loading } from "@components/Loading";
 
 import { SignUp } from "@screens/SignUp";
+import { SignIn } from "@screens/SignIn";
+import { AuthRoutes } from "src/Routes/auth.routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -20,7 +22,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
