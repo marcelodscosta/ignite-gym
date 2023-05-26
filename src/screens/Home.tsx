e2dcb3d3bc4050d8cfd646ebf88eea30b05a937d
Marcelo Costa <marcelodscosta@yahoo.com.br>
2023-05-26 15:08:24 -0300

    Finalizing the Interfaces - Listing the groups

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,35 @@
-import { VStack } from "native-base";
+import { useState } from "react";
+
+import { HStack, VStack, FlatList } from "native-base";
 
 import { HomeHeader } from "@components/HomeHeader";
+import { Group } from "@components/Group";
 
 export const Home = () => {
+  const [groupSelected, setGroupSelected] = useState("costa");
+  const [groups, setGroup] = useState(["costa", "bíceps", "tríceps", "ombro"]);
+
   return (
     <VStack flex={1} bg="gray.700">
       <HomeHeader />
+      <HStack>
+        <FlatList
+          data={groups}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Group
+              name={item}
+              isActive={groupSelected === item}
+              onPress={() => setGroupSelected(item)}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          _contentContainerStyle={{ px: 8 }}
+          my={10}
+          maxH={10}
+        />
+      </HStack>
     </VStack>
   );
 };
