2e326fabe1444b3c312dd7e1fa8da0f261051794
marcelodscosta <81534727+marcelodscosta@users.noreply.github.com>
2023-06-01 16:32:20 -0300

    Finalizing the Profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -4,6 +4,8 @@ import { TouchableOpacity } from "react-native";
 
 import { Heading, ScrollView, Skeleton, VStack } from "native-base";
 
+import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
 
@@ -20,12 +22,34 @@ export const Profile = () => {
       <ScreenHeader title="Perfil" />
       <ScrollView>
         <VStack alignItems="center">
-          <Skeleton w={33} h={33} rounded="full" isLoaded={isLoaded} />
+          <Skeleton
+            w={33}
+            h={33}
+            rounded="full"
+            isLoaded={isLoaded}
+            startColor="gray.400"
+            endColor="gray.300"
+          />
           {isLoaded && < UserPhoto size={148} source={{ uri: "https://www.github.com/marcelodscosta.png" }} alt="Foto de Perfil" />}
           <TouchableOpacity>
             <Heading fontSize="xl" color="green.500" mt={4}>Alterar Foto</Heading>
           </TouchableOpacity>
+          <VStack w="88%" mt={8}>
+            <Input placeholder="Nome" bg="gray.600" />
+            <Input keyboardType="email-address" placeholder="E-mail" bg="gray.600" />
+          </VStack>
+
+          <VStack w="88%" mt={1}>
+            <Heading color="gray.100" fontSize="md" alignSelf="flex-start" mb={2}>
+              Alterar Senha
+            </Heading>
+            <Input secureTextEntry placeholder="Senha antiga" bg="gray.600" />
+            <Input secureTextEntry placeholder="Nova senha" bg="gray.600" />
+            <Button title="Atualizar" />
+          </VStack>
+
         </VStack>
+
       </ScrollView>
     </VStack>
   );
