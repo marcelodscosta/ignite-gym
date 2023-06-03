44e60104aa2861e3d63f49fd2d97909b61e1b7f9
marcelodscosta <81534727+marcelodscosta@users.noreply.github.com>
2023-06-02 21:14:29 -0300

    Installing react-hook-form

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,13 +3,12 @@ import { Box } from "native-base";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { AuthRoutes } from "./auth.routes";
-import { AppRoutes } from "./app.routes";
 
 export const Routes = () => {
   return (
     <Box flex={1} bg="gray.700">
       <NavigationContainer>
-        <AppRoutes />
+        <AuthRoutes />
       </NavigationContainer>
     </Box>
   );
